e104d32f13c4290bedbda2574a0639d939685bf6
IljaDR <38908439+IljaDR@users.noreply.github.com>
2021-01-24 23:47:31 +0100

    Removed sanitize

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const productDAO = require('../../lib/models/productDAO.js');
-const sanitizeHtml = require('sanitize-html');
 
 router.get('/', async (req, res) => {
    let products = await productDAO.getAllProducts();
