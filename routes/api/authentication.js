465e30be644a73b26f484242d073c7479bbc49f7
IljaDR <38908439+IljaDR@users.noreply.github.com>
2021-02-28 20:19:44 +0100

    Changed lib requirement
    Changed bcrypt to bcryptjs, since it the former doesn't appear to be supported anymore

--- routes/api/authentication.js
+++ routes/api/authentication.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const authenticationDAO = require('../../lib/models/authenticationDAO.js');
 const passport = require('passport');
 
